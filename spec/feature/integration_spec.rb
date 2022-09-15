85197cc947f43e603d14aa89ae97c11fa223d480
David King <davidking@tamu.edu>
2022-09-15 16:55:19 -0500

    Added unit and integration tests for new atributes; author, price, pubDate

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'author', with: 'jk rowling'
+    fill_in 'price', with: 60
+    select "2022", :from => "published_date_1i"
+    select "March", :from => "published_date_2i"
+    select "12", :from => "published_date_3i"
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
